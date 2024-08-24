27f422f88a0ce6bde3a88409449822c05eefd1ce
jianswe <kuangjian89@outlook.com>
2024-08-24 10:14:22 -0500

    BST

--- tst/BST.test.ts
+++ tst/BST.test.ts
@@ -15,3 +15,20 @@ test("insert and traverseInOrder: build a tree from scratch", () => {
         expect(nums[i]).toBe(i+1)
     } 
 })
+
+test("Input: root = [10,5,15,3,7,18]", () => {
+    const expected = [3,5,7,10,15,18]
+    let bst = new BST()
+    bst.insert(10)
+    bst.insert(5)
+    bst.insert(15)
+    bst.insert(3)
+    bst.insert(7)
+    bst.insert(18)
+    const nums = bst.traverseInOrder()
+
+    expect(nums.length).toBe(6)
+    for (let i=0; i<nums.length; i++) {
+        expect(nums[i]).toBe(expected[i])
+    } 
+})
